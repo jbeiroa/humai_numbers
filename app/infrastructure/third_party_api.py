f2edb1714228727584a5c8f5524c17703e531af6
Juan Ignacio Beiroa <jbeiroa@users.noreply.github.com>
2023-10-03 21:18:03 -0300

    pylinted third_party_api.py

---
+++ app/infrastructure/third_party_api.py
@@ -0,0 +1,24 @@
+"""REST API using numbersapi in Python
+
+    Returns:
+        str: number fact of a number
+"""
+
+import requests
+
+def get_number_fact(number) -> str:
+    """Get a fact of a requested number
+
+    Args:
+        number (int): a number to get information
+        information (_type_): _description_
+
+    Returns:
+        str: a fact of the requested number
+    """
+    url = "http://numbersapi.com/" + str(number)
+    response = requests.get(url, timeout=10)
+    if response.status_code == 200:
+        fact = response.text
+        return fact
+    return "There was an error."
